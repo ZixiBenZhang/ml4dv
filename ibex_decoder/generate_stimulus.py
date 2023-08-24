6acf20dfabf6ec09fff77dcd1cf3dd781f3aca13
ZixiBenZhang <13126817936@163.com>
2023-08-24 13:35:07 +0100

    Testing ID with ChatGPT and IDADAS bin-sampling Template1, STABLE reset best_iters, Best3 sampling-from-bests, ORDINARY restart plan

--- ibex_decoder/generate_stimulus.py
+++ ibex_decoder/generate_stimulus.py
@@ -55,7 +55,7 @@ def main():
     # build components
     prompt_generator = TemplatePromptGenerator4ID1(
         bin_descr_path="../examples_ID/bins_description.txt",
-        sampling_missed_bins_method="IDNEWEST",
+        sampling_missed_bins_method="IDADAS",
     )
 
     # stimulus_generator = Llama2(system_prompt=prompt_generator.generate_system_prompt())
