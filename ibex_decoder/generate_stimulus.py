049d346388b1b1aec4eeb2c9fe7dccd72cbed827
ZixiBenZhang <13126817936@163.com>
2023-08-28 12:09:34 +0100

    Testing ID with ChatGPT: try to reach full coverage Template2, IDAdaNew bin-sampling, STABLE reset best_iters, Best3 sampling-from-bests, IDADAR restart plan Dialog_bound=1000

--- ibex_decoder/generate_stimulus.py
+++ ibex_decoder/generate_stimulus.py
@@ -55,7 +55,7 @@ def main():
     # build components
     prompt_generator = TemplatePromptGenerator4ID2(
         bin_descr_path="../examples_ID/bins_description.txt",
-        sampling_missed_bins_method="IDADAS",
+        sampling_missed_bins_method="IDAdaNew",
     )
 
     # stimulus_generator = Llama2(system_prompt=prompt_generator.generate_system_prompt())
@@ -81,7 +81,7 @@ def main():
         extractor,
         stimulus_filter,
         [logger_txt, logger_csv],
-        dialog_bound=300,
+        dialog_bound=1000,
         rst_plan=rst_plan_IDADAR,
     )
     print("Agent successfully built\n")
