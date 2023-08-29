29b6bdc83a9c6253ba83566babeecc3984ade99a
ZixiBenZhang <13126817936@163.com>
2023-08-29 16:22:05 +0100

    Testing ID with ChatGPT: full coverage harder-bin sampling, Template1, IDNewest bin-sampling, Best3 sampling-from-bests, ORDINARY restart plan Dialog_bound=1000

--- ibex_decoder/generate_stimulus.py
+++ ibex_decoder/generate_stimulus.py
@@ -53,9 +53,9 @@ def main():
     )
 
     # build components
-    prompt_generator = TemplatePromptGenerator4ID2(
+    prompt_generator = TemplatePromptGenerator4ID1(
         bin_descr_path="../examples_ID/bins_description.txt",
-        sampling_missed_bins_method="IDAdaNew",
+        sampling_missed_bins_method="IDNewest",
     )
 
     # stimulus_generator = Llama2(system_prompt=prompt_generator.generate_system_prompt())
@@ -82,7 +82,7 @@ def main():
         stimulus_filter,
         [logger_txt, logger_csv],
         dialog_bound=1000,
-        rst_plan=rst_plan_IDADAR,
+        rst_plan=rst_plan_ORDINARY,
     )
     print("Agent successfully built\n")
 
