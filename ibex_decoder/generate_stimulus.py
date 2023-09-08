5ac0bbb07c1b2780793a1100012c54d63583e22d
ZixiBenZhang <13126817936@163.com>
2023-09-08 11:37:55 +0100

    Testing ID with ChatGPT: ask for long response when warmed-up Updated warmed-up check harder-bin sampling, Template3, IDAdaNew bin-sampling, Best3 sampling-from-bests, IDAdaR restart plan Dialog_bound=800

--- ibex_decoder/generate_stimulus.py
+++ ibex_decoder/generate_stimulus.py
@@ -54,7 +54,7 @@ def main():
     )
 
     # build components
-    prompt_generator = TemplatePromptGenerator4ID2(
+    prompt_generator = TemplatePromptGenerator4ID3(
         bin_descr_path="../examples_ID/bins_description.txt",
         sampling_missed_bins_method="IDAdaNew",
     )
@@ -82,7 +82,7 @@ def main():
         extractor,
         stimulus_filter,
         [logger_txt, logger_csv],
-        dialog_bound=400,
+        dialog_bound=800,
         rst_plan=rst_plan_IDADAR,
     )
     print("Agent successfully built\n")
