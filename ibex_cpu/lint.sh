245c6e5556dffedeea8bdfb228f3116bae16e891
Hugo McNally <hugo.mcnally@gmail.com>
2023-09-19 17:14:23 +0100

    ibex_cpu: added linting configurations and script

---
+++ ibex_cpu/lint.sh
@@ -0,0 +1,11 @@
+#!/bin/env sh
+set -e
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" > /dev/null && pwd -P)"
+cd "$SCRIPT_DIR"
+
+echo "$0: Running flake8"
+flake8
+
+echo "$0: Running mypy"
+mypy ./*.py
