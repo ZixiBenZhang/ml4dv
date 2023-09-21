22b8bbcb9dfac05378f34c16f1fa7ee1db3783b9
ZixiBenZhang <13126817936@163.com>
2023-09-21 09:33:51 +0100

    Added bin description, undone

--- agents/agent_IC_dumb.py
+++ agents/agent_IC_dumb.py
@@ -2,6 +2,51 @@ from agents.agent_base import *
 from ibex_cpu.shared_types import IbexStateInfo
 
 
+# 00100080 00a00293
+# 00100084 01400313
+# 00100088 00000393
+# 0010008c 006383b3
+# 00100090 fff28293
+# 00100094 fe029ce3
+# 0010008c 006383b3
+# 00100090 fff28293
+# 00100094 fe029ce3
+# 0010008c 006383b3
+# 00100090 fff28293
+# 00100094 fe029ce3
+# 0010008c 006383b3
+# 00100090 fff28293
+# 00100094 fe029ce3
+# 0010008c 006383b3
+# 00100090 fff28293
+# 00100094 fe029ce3
+# 0010008c 006383b3
+# 00100090 fff28293
+# 00100094 fe029ce3
+# 0010008c 006383b3
+# 00100090 fff28293
+# 00100094 fe029ce3
+# 0010008c 006383b3
+# 00100090 fff28293
+# 00100094 fe029ce3
+# 0010008c 006383b3
+# 00100090 fff28293
+# 00100094 fe029ce3
+# 0010008c 006383b3
+# 00100090 fff28293
+# 00100094 fe029ce3
+# 00100098 0000006f
+# add seen: 10, zero_dst: 0, zero_src: 0, same_src: 0
+# sub seen: 0, zero_dst: 0, zero_src: 0, same_src: 0
+# sll seen: 0, zero_dst: 0, zero_src: 0, same_src: 0
+# slt seen: 0, zero_dst: 0, zero_src: 0, same_src: 0
+# sltu seen: 0, zero_dst: 0, zero_src: 0, same_src: 0
+# xor seen: 0, zero_dst: 0, zero_src: 0, same_src: 0
+# srl seen: 0, zero_dst: 0, zero_src: 0, same_src: 0
+# sra seen: 0, zero_dst: 0, zero_src: 0, same_src: 0
+# or seen: 0, zero_dst: 0, zero_src: 0, same_src: 0
+# and seen: 0, zero_dst: 0, zero_src: 0, same_src: 0
+
 class DumbAgent4IC(BaseAgent):
     def __init__(self):
         super().__init__()
@@ -22,4 +67,3 @@ class DumbAgent4IC(BaseAgent):
         self, dut_state: GlobalDUTState, coverage_database: GlobalCoverageDatabase
     ) -> List[Tuple[int, int]]:
         return []
-
