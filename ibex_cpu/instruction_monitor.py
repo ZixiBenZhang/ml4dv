4c1756e7d476114433f8f0ac6d6a0ff9f3c58edc
Hugo McNally <hugo.mcnally@gmail.com>
2023-09-26 06:48:37 +0100

    ibex_cpu: Added raw hazard coverage.

--- ibex_cpu/instruction_monitor.py
+++ ibex_cpu/instruction_monitor.py
@@ -8,7 +8,7 @@ class InstructionMonitor:
         self.insn_valid = dut.u_top.rvfi_valid
         self.insn_pc = dut.u_top.rvfi_pc_rdata
         self.insn = dut.u_top.rvfi_insn
-        self.coverage_db = CoverageDatabase(instructions={})
+        self.coverage_db = CoverageDatabase(instructions={}, cross_coverage={})
         self.last_pc = None
         self.last_insn = None
 
@@ -17,6 +17,10 @@ class InstructionMonitor:
                 cov: 0
                 for cov in instr.type().coverpoints()
             }
+            self.coverage_db.cross_coverage[instr] = {
+                (other_instr, cov): 0
+                for (other_instr, cov) in instr.type().cross_coverpoints()
+            }
 
     def sample_insn_coverage(self):
         if self.insn_valid.value == 0:
@@ -24,12 +28,19 @@ class InstructionMonitor:
             self.last_insn = None
             return
 
-        self.last_pc = int(self.insn_pc.value)
-        self.last_insn = int(self.insn.value)
-
         insn = Encoding(self.insn.value).typed()
 
         if insn is not None:
             mnemonic = insn.instruction()
             for coverpoint in insn.sample_coverage():
                 self.coverage_db.instructions[mnemonic][coverpoint] += 1
+
+            if (
+                self.last_insn is not None and
+                (last_insn := Encoding(self.last_insn.value).typed()) is not None
+            ):
+                for insn_cov in insn.sample_cross_coverage(last_insn):
+                    self.coverage_db.instructions[mnemonic][insn_cov] += 1
+
+        self.last_pc = int(self.insn_pc.value)
+        self.last_insn = int(self.insn.value)
--- ibex_cpu/shared_types.py
+++ ibex_cpu/shared_types.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from itertools import chain
 from collections import OrderedDict
 from typing import Optional
 
@@ -8,13 +9,20 @@ from instructions import Instr, Cov
 @dataclass
 class CoverageDatabase:
     instructions: dict[Instr, dict[Cov, int]]
+    cross_coverage:  dict[Instr, dict[tuple[Instr, Cov], int]]
 
     def get_coverage_dict(self) -> OrderedDict[str, int]:
-        generator = (
+        cov_generator = (
             (f"{instr.value}_{cov.value}", num)
             for (instr, covs) in self.instructions.items()
             for (cov, num) in covs.items()
         )
+        xcov_generator = (
+            (f"{prev_instr.value}->{instr.value}_{cov.value}", num)
+            for (instr, x_covs) in self.cross_coverage.items()
+            for ((prev_instr, cov), num) in x_covs.items()
+        )
+        generator = chain(cov_generator, xcov_generator)
         return OrderedDict(sorted(generator, key=lambda cov: cov[0]))
 
     def get_coverage_vector(self) -> list[int]:
@@ -26,11 +34,24 @@ class CoverageDatabase:
 
     def output(self):
         for (instr, covs) in self.instructions.items():
-            print(f'{instr.value}:')
             print(
-                ' ' * 4,
-                ', '.join(f'{cov.value}: {num}' for (cov, num) in covs.items())
+                f'{instr.value}: (',
+                ', '.join(f'{cov.value} {num}' for (cov, num) in covs.items()),
+                ')',
             )
+
+        raw_hazards = (
+            f'{prev_instr.value}->{instr.value} {num}'
+            for (instr, x_covs) in self.cross_coverage.items()
+            for ((prev_instr, cov), num) in x_covs.items()
+            if cov == Cov.RAW_HAZARD
+        )
+        WIDTH = 5
+        print('Raw Hazards: (\n  ', end='')
+        for (idx, item) in enumerate(raw_hazards):
+            trailing = ',\n  ' if not (idx + 1) % WIDTH else ',\t'
+            print(item, end=trailing)
+        print('\n)')
 
 
 @dataclass
